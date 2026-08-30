311859b5827db8d83edad0e662b556b8ac267338
Francis <mewsfrancisco61@gmail.com>
2026-08-30 16:28:59 -0300

    Resolvendo ordem dos exercicios

--- Atividades/Atividade 3/exercicio-03/src/App.jsx
+++ Atividades/Atividade 3/exercicio-03/src/App.jsx
@@ -1,92 +1,25 @@
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="2">{category}</th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? (
-    product.name
-  ) : (
-    <span style={{ color: 'red' }}>{product.name}</span>
-  );
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products }) {
-  const rows = [];
-
-  let productcategory = [...new Set(products.map((product) => product.category))];
-
-  productcategory.map((category) => {
-    rows.push(
-      <ProductCategoryRow
-        category={category}
-        key={category}
-      />
-    );
-
-    const produtosDestaCategoria = products.filter(
-      (product) => product.category === category
-    );
-
-    produtosDestaCategoria.map((product) => {
-      rows.push(<ProductRow product={product} key={product.name} />);
-    });
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" /> Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
-  );
-}
+import './App.css';
+import ProductTable from './components/ProductTable/ProductTable';
+import SearchBar from './components/SearchBar/SearchBar.jsx';
 
 const PRODUCTS = [
-  { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
-  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
-  { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
-  { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
-  { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
-  { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
-];
+    { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
+    { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
+    { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
+    { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
+    { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
+    { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
+  ];
 
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable products={PRODUCTS} />
+    </>
+  );
 }
+
+
+export default App;
---
+++ Atividades/Atividade 3/exercicio-03/src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+import ProductTable from '../ProductTable/ProductTable';
+import SearchBar from '../SearchBar/SearchBar';
+
+export default function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
